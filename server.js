35b1d424bfe7cec1150e207b1ad8c4dab1fbec49
Natalie Mall <nkmall@ucdavis.edu>
2020-07-14 21:39:06 -0700

    Working version (some combo of sqlite and postgres)

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const sqlite3 = require('sqlite3').verbose();
 
 const DB_PATH = './database/sqlite3';
 
+
+const PORT = process.env.PORT || 5000;  // added to try to get a local port
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -10,6 +12,17 @@ const pool = new Pool({
   }
 });
 
+
+// //added on July 13 to try to adjust database from server.js
+// pool.connect();
+
+// pool.query('SELECT pubic,test_table FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   pool.end();
+// });
 
 
 const express = require('express');
@@ -24,7 +37,7 @@ async function startApp() {
     // router.use(bodyParser.json());
 
 
-    const PORT = process.env.PORT || 4002;
+    // const PORT = process.env.PORT || 4002;
 
     app.use(express.static('public'));
 
@@ -50,17 +63,24 @@ async function startApp() {
 
       // console.log(db)
 
+      // try {
+      //   const client = await pool.connect(); 
+      //   const result = await client.query('SELECT * FROM test_table');
+      //   const results = { 'results': (result) ? result.rows : null};
+      //   res.send( results );
+      //   client.release();
+      // } catch (err) {
+      //   console.error(err);
+      //   res.send("Error " + err);
+      // }
+
+      // commented out because it's sqlite stuff: (july 14 2020)
       db.all('SELECT message FROM messages ORDER BY date DESC, time DESC Limit 10;', (err, rows) => {
       // db.all('SELECT date, time, message FROM messages;', (err, rows) => {
 
         console.log(rows)
         res.send(rows)
 
-        // Just send 10 most recent rows? 
-        // console.log(rows)
-        // res.send('test1');
-
-        // TODO error handling
       });
     });
 
@@ -161,7 +181,7 @@ async function startApp() {
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
-    }
+    } 
     })
 
 
